1c92d098f73c9586d1756b9beeba6c25c3ef5eee
jri8 <jri8@georgetown.edu>
2017-03-30 15:42:27 -0400

    Fix error with substring and NaN

--- BallPanel.java
+++ BallPanel.java
@@ -21,72 +21,72 @@ import java.lang.Math;
 
 public class BallPanel extends JPanel implements MouseListener, KeyListener{
 
-	
-	
+
+
 	ArrayList<Ball> balls = new ArrayList<Ball>();
-	
+
 	boolean barrierUp;
-	
+
 	public BallPanel()
 	{
 		this.setBackground(Color.PINK);
-		for(int i = 0; i < 5; i++)
+		for(int i = 0; i < 1; i++)
 			balls.add(new Ball());
 		System.out.println(balls.size());
-		
+
 		addMouseListener(this);
 		addKeyListener(this);
-		
+
 		barrierUp = true;
-		
-		
+
+
 		start();
 	}
-	
+
 	public void start()
 	{
 		Thread t = new Thread()
+		{
+			public void run()
+			{
+				while (true)
 				{
-					public void run()
+					moveBalls();
+					repaint();
+
+					try
 					{
-						while (true)
-						{
-							moveBalls();
-							repaint();
-							
-							try
-							{
-								Thread.sleep(40);
-							}
-							
-							catch (InterruptedException e)
-							{
-								
-							}
-							
-						}
+						Thread.sleep(40);
 					}
-				} ;
-				
-				t.start();
+
+					catch (InterruptedException e)
+					{
+
+					}
+
+				}
+			}
+		} ;
+
+		t.start();
 	}
-	
-	
+
+
 	public void moveBalls()
 	{
 		for (Ball b : balls)
 			b.move();
 	}
-	
+
 	public void paint(Graphics g)
 	{
 		super.paint(g);
-		
+
 		for (Ball b : balls)
 		{
 			b.paint(g);
 		}
-		
+
 		if (barrierUp)
 		{
 			g.setColor(Color.BLACK);
@@ -98,25 +98,25 @@ public class BallPanel extends JPanel implements MouseListener, KeyListener{
 			g.drawLine(400, 0, 400, 300);
 			g.drawLine(400, 800, 400, 500);
 		}
-		
+
 		Graphics2D g2 = (Graphics2D) g;
 		g.setColor(Color.BLACK);
-        g2.setStroke(new BasicStroke(10));
+		g2.setStroke(new BasicStroke(10));
 		g2.drawLine(0, 0, 0, 800);
 		g2.drawLine(0, 800, 800, 800);
 		g2.drawLine(800, 800, 800, 0);
 		g2.drawLine(800, 0, 0, 0);
-		
+
 		g2.setFont(new Font("TimesRoman", Font.PLAIN, 40));
 		g2.drawString(getRightSpeed(), 600, 100);
 		g2.drawString(getLeftSpeed(), 200, 100);
-		
-		
+
+
 	}
-	
+
 	public String getLeftSpeed()
 	{
-		String str = "";
+		String str = "0";
 		Double leftSideSpeed = 0.0;
 		int count = 0;
 		for (Ball b : balls)
@@ -127,16 +127,20 @@ public class BallPanel extends JPanel implements MouseListener, KeyListener{
 				count++;
 			}
 		}
-		
+
 		leftSideSpeed /= count;
-		str = leftSideSpeed.toString();
-		str = str.substring(0,4);
+
+		if (count != 0)
+		{
+			str = leftSideSpeed.toString();
+			str = str.substring(0,4);
+		}
 		return str;
 	}
-	
+
 	public String getRightSpeed()
 	{
-		String str = "";
+		String str = "0";
 		Double rightSideSpeed = 0.0;
 		int count = 0;
 		for (Ball b : balls)
@@ -147,34 +151,38 @@ public class BallPanel extends JPanel implements MouseListener, KeyListener{
 				count++;
 			}
 		}
-		
+
 		rightSideSpeed /= count;
-		str = rightSideSpeed.toString();
-		str = str.substring(0,4);
+		if (count != 0)
+		{
+			str = rightSideSpeed.toString();
+			str = str.substring(0,4);
+		}
+		
 		return str;
 	}
-	
+
 
 	public static void main(String[] args) {
-		
+
 		JFrame frame = new JFrame("Maxwell's Demons");
 		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 		frame.setDefaultLookAndFeelDecorated(true);
 		frame.setSize(900,900);
 		frame.setLayout(new BorderLayout(10, 10));
-		
+
 		BallPanel p = new BallPanel();
-	
-		
-		
+
+
+
 		frame.add(p, BorderLayout.CENTER);
-	//	frame.add(new JButton(), BorderLayout.NORTH);
-	//	frame.add(new JButton(), BorderLayout.WEST);
+		//	frame.add(new JButton(), BorderLayout.NORTH);
+		//	frame.add(new JButton(), BorderLayout.WEST);
 
 		frame.setVisible(true);
-		
-		
-		
+
+
+
 	}
 
 
@@ -182,8 +190,8 @@ public class BallPanel extends JPanel implements MouseListener, KeyListener{
 		// TODO Auto-generated method stub
 		//balls.add(new Ball());
 		//System.out.println("Mouse clicked");
-		
-		
+
+
 	}
 
 	public void mousePressed(MouseEvent e) {
@@ -193,30 +201,30 @@ public class BallPanel extends JPanel implements MouseListener, KeyListener{
 		{
 			b.barrierUp = false;
 		}
-		
+
 	}
 
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
 		barrierUp = true;
-		
+
 		for (Ball b: balls)
 		{
 			b.barrierUp = true;
 		}
-		
+
 	}
 
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
 
-		
+
 	}
 
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
 
-		
+
 	}
 
 	public void keyTyped(KeyEvent e) {
@@ -225,8 +233,8 @@ public class BallPanel extends JPanel implements MouseListener, KeyListener{
 		char character = '?';
 		character = e.getKeyChar();
 		System.out.println(character);
-		
-		
+
+
 	}
 
 	public void keyPressed(KeyEvent e) {
@@ -237,10 +245,10 @@ public class BallPanel extends JPanel implements MouseListener, KeyListener{
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
 		System.out.println(e.getKeyChar());
-		
+
 	}
-	
-	
-	
-	
+
+
+
+
 }
